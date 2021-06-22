119b73dac2b916891a9ed5cdc345e7febea4d174
Andrejus Timko <timcostudios@gmail.com>
2021-06-22 09:14:18 +0300

    changes2

---
+++ 001/validaion/src/App.jsx
@@ -0,0 +1,12 @@
+import MainForm from './components/mainForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <MainForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ 001/validaion/src/components/mainForm.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class MainForm extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="main-form">
+                <h1>Main Form</h1>
+            </div>
+         );
+    }
+}
+ 
+export default MainForm;
